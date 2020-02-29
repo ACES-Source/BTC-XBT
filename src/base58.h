8774e6085e0a5c3e00bb8015cab3e356be1872b9
ACES-Source <55683173+ACES-Source@users.noreply.github.com>
2020-02-29 01:29:01 -0500

    Update base58.h

--- src/base58.h
+++ src/base58.h
@@ -1,5 +1,6 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
 // Copyright (c) 2009-2019 The Bitcoin Core developers
+// Copyright (C) 2020-2022 ACES ALLIANZ
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
